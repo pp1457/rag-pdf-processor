b4dc80a49a6729d2a3b1f6b9146b5a8bb8cf17e3
Paul Liao <liaoyunyang0815@gmail.com>
2024-08-20 11:15:13 +0800

    complete most of with_pdfplumber.py

--- pdf_to_markdown/with_pdfplumber.py
+++ pdf_to_markdown/with_pdfplumber.py
@@ -1,13 +1,21 @@
 import pdfplumber
 from collections import Counter
+from typing import List
 
-def extract_header_fontsize_from_pdf(pdf_path):
+def extract_lines(pdf_path):
 
     font_size_counter = Counter()
 
     content = ""
 
-    line_sizes = [0]
+    line_metadatas = [{
+        "font_size": 0,
+        "page": 0,
+        "line_id": 0,
+        "file_path": pdf_path
+    }]
+
+    lines = [""]
     line_id = 0
 
     with pdfplumber.open(pdf_path) as pdf:
@@ -23,68 +31,118 @@ def extract_header_fontsize_from_pdf(pdf_path):
             for char in chars:
                 if char["top"] > last_bottom:
                     if line_id > 0:
-                        
-                        if tmp_counter:
-                            line_sizes.append(tmp_counter.most_common()[0][0])
-                        else:
-                            line_sizes.append(0)
 
-                        font_size_counter[line_sizes[line_id]] += 1
+                        cur_font_size = 0
+
+                        if tmp_counter:
+                            cur_font_size = tmp_counter.most_common()[0][0]
+                        
+                        line_metadatas.append({
+                            "font_size": cur_font_size,
+                            "page": pdf.pages[i].page_number,
+                            "line_id": line_id+1,
+                            "file_path": pdf_path
+                        })
+                        lines.append(line_content + "\n")
+
+                        font_size_counter[cur_font_size] += 1
 
                         tmp_counter.clear()
                         line_content = ""
 
                     line_id += 1
-                    content += "\n"
 
-                tmp_counter[int(char["size"])] += 1
                 line_content += char["text"]
-
+                tmp_counter[int(char["size"])] += 1
                 last_bottom = char["bottom"]
-                content += char["text"]
+            
+            cur_font_size = 0
+
+            if tmp_counter:
+                cur_font_size = tmp_counter.most_common()[0][0]
+            
+            line_metadatas.append({
+                "font_size": cur_font_size,
+                "page": pdf.pages[i].page_number,
+                "line_id": line_id+1,
+                "file_path": pdf_path
+            })
+            lines.append(line_content + "\n")
 
         most_common_sizes = tmp_counter.most_common()
         font_size_counter[most_common_sizes[0][0]] += 1
-        
-    lines = content.splitlines()
+    
+#    for line in lines:
+#        print("-----")
+#        print(line)
 
-#     for line in lines:
-#         print("-----")
-#         print(line)
+#    print(font_size_counter.most_common())
 
-    print(font_size_counter.most_common())
+#    print(font_size_counter.items())
 
-    repeated_sizes = [size for size, count in font_size_counter.items() if count > 1]
+    repeated_sizes = [size for size, count in font_size_counter.items() if count > 2]
+
     repeated_sizes.sort(reverse=True)
+    
+    header_sizes = []
 
-    return lines, line_sizes 
+    for i in range(6):
+        if i >= len(repeated_sizes) or repeated_sizes[i] == 0:
+            break
+        header_sizes.append(repeated_sizes[i])
+
+#    print(header_sizes)
+
+    return lines, line_metadatas, header_sizes
+
+def add_hash_to_header_lines(lines, line_metadatas, header_sizes):
+
+    md_pages = []
+
+    page_content = ""
+    last_page = -1
+
+    for i in range(len(lines)):
+        if not i:
+            continue
+
+        cur_font_size = line_metadatas[i]["font_size"]
+        # print("cur_font_size: ")
+        # print(cur_font_size)
+        cur_page = line_metadatas[i]["page"]
+
+        if (not last_page == -1) and (not cur_page == last_page):
+            md_pages.append({
+                "text": page_content,
+                "metadata": {
+                    "file_path": line_metadatas[i]["file_path"],
+                    "page": last_page
+                },
+                "line_id": line_metadatas[i]["line_id"]
+            })
+            page_content = ""
+
+        if cur_font_size in header_sizes:
+            number_of_hashes = header_sizes.index(cur_font_size) + 1
+            for _ in range(number_of_hashes):
+                lines[i] = "#" + lines[i]
+            # print(lines[i])
+
+        page_content += lines[i]
+
+        last_page = cur_page
+
+    return md_pages
 
 
+def pdf_to_markdown(file_name: str) -> List[dict]:
+    input_file = "data/" + file_name + ".pdf"
+    lines, line_metadatas, header_sizes = extract_lines(input_file)
 
+    result = add_hash_to_header_lines(lines, line_metadatas, header_sizes)
 
-def extract_lines_with_font_size(pdf_path, target_font_size):
-    lines_with_target_font_size = []
-
-    with pdfplumber.open(pdf_path) as pdf:
-        for i in range(len(pdf.pages)):
-            words = pdf.pages[i].extract_words(extra_attrs=['fontname', 'size'])
-            lines = {}
-
-            for word in words:
-                line_num = word['top']
-                if line_num not in lines:
-                    lines[line_num] = []
-                lines[line_num].append(word)
-
-            for line_num, line_words in lines.items():
-                line_font_sizes = [word['size'] for word in line_words]
-                if target_font_size in line_font_sizes:
-                    line_text = ' '.join([word['text'] for word in line_words])
-                    lines_with_target_font_size.append(line_text)
-
-    return lines_with_target_font_size
-
+    return result
 
 
 if __name__ == "__main__":
-     extract_header_fontsize_from_pdf("data/report.pdf")
+    pdf_to_markdown("game")
